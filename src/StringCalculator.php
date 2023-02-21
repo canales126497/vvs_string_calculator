191fcb72c6272662d89290880980c81dba44fb61
David Canales Parra <canalesparradavid@gmail.com>
2023-02-21 12:08:32 +0000

    Permite al metodo add sumar cualquier cantidad de numeros

---
+++ src/StringCalculator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Deg540\PHPTestingBoilerplate;
+
+class StringCalculator{
+    function add(string $numbers_to_add):int
+    {
+        if(empty($numbers_to_add))
+            return 0;
+
+        $numbers_to_add_array = explode(",", $numbers_to_add);
+        $number_of_items_to_add = count($numbers_to_add_array);
+
+        $added_number = 0;
+        for($i = 0; $i < $number_of_items_to_add; $i++)
+        {
+            $added_number += intval($numbers_to_add_array[$i]);
+        }
+
+        return $added_number;
+    }
+}
---
+++ tests/StringCalculatorTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Deg540\PHPTestingBoilerplate\Test;
+
+use Deg540\PHPTestingBoilerplate\StringCalculator;
+use Deg540\PHPTestingBoilerplate\StringCalculatorTest1;
+use PHPUnit\Framework\TestCase;
+
+class StringCalculatorTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->calculadora = new StringCalculator();
+    }
+    /**
+     * @test
+     **/
+    function returns0OnEmpty()
+    {
+        $suma_de_numeros = $this->calculadora->add("");
+
+        $this->assertEquals(0, $suma_de_numeros);
+    }
+
+    /**
+     * @test
+     */
+    function makesSumOf2Numbers()
+    {
+        $suma_de_numeros = $this->calculadora->add("1,2");
+
+        $this->assertEquals(3, $suma_de_numeros);
+    }
+
+    /**
+     * @test
+     */
+    function makesSumOf3Numbers()
+    {
+        $suma_de_numeros = $this->calculadora->add("1,2,2");
+
+        $this->assertEquals(5, $suma_de_numeros);
+    }
+
+    /**
+     * @test
+     */
+    function makesSumOfMoreThan3NUmbers()
+    {
+        $suma_de_numeros = $this->calculadora->add("1,2,3,4,5,6");
+
+        $this->assertEquals(21, $suma_de_numeros);
+    }
+}
